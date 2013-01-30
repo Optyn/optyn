c39b0164c0e75c993d5ef3db4f300a267a357e0c
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-01-30 20:12:39 +0530

    Adding a new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optyn::Application.config.secret_token = '102968b6f0830163057b7d78dcef52cc727dce70669d25f345d5011760c248b77a34bb34fde7702d55792717ecea0a1ad06830a0182810bce5d7bc0c629a2ea5'
