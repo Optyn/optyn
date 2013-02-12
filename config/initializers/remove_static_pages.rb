ce4390fb5426833b418d29e8b6f84081375313ee
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-02-12 14:59:42 +0530

    Don't include engine front_end and back_end static pages HTML and CSS in the assets except development environment

--- config/initializers/remove_static_pages.rb
+++ config/initializers/remove_static_pages.rb
@@ -1,5 +1,7 @@
 class Engine < Rails::Engine
-   initializer "remove assets directories from pipeline" do |app|
-     app.config.assets.paths = app.config.assets.paths - app.config.assets.paths.grep(/back_end_static_page.*|front_end_static_page.*/)
-   end
+	unless "development" == Rails.env
+		initializer "remove assets directories from pipeline" do |app|
+			app.config.assets.paths = app.config.assets.paths - app.config.assets.paths.grep(/back_end_static_page.*|front_end_static_page.*/)
+		end
+	end
 end
