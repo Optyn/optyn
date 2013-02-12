481c62fd2cc0732d8a2b346f54665d644b4aa7bc
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-02-12 20:23:10 +0530

    Print the rails env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :print_rails_env
+
+  private
+  def print_rails_env
+  	puts "--- Rails Env: #{Rails.env}"
+  end
 end
