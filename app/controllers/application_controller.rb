5dd632f4704363a07f70d72401ce84ea20c6e351
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-02-13 20:14:15 +0530

    Remove a debugging before filter to print the env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :print_rails_env
-
-  private
-  def print_rails_env
-  	Rails.logger.info "--- Rails Env: #{Rails.env}"
-  end
 end
