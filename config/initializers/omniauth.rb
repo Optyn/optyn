f45521ecb2c428928289a6b1b262220016865d3e
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-02-19 15:07:23 +0530

    Move the twitter oauth credentials in the config file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 	# provider :developer unless Rails.env.production?
-  # provider :twitter, 'Acwx2jBkCCbFZHkW8wEubQ', 'zGevh2gWCvuW2h2BXPe84arg5sm7A4bEWmxvHddno'
-  provider :twitter, 'Acwx2jBkCCbFZHkW8wEubQ', 'zGevh2gWCvuW2h2BXPe84arg5sm7A4bEWmxvHddno'
+  provider :twitter, SiteConfig.twitter_consumer_key, SiteConfig.twitter_consumer_secret
   # provider :facebook, SiteConfig.facebook_app_id, SiteConfig.facebook_app_secret
   # provider :google_oauth2, SiteConfig.google_client_id, SiteConfig.google_client_secret
 
