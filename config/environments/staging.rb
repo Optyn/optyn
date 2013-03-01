9343e89fbedf3911e3bdf969a93457f9cce6eaa4
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-03-01 15:25:29 +0530

    Ignore file list for fornt_end and back_end list

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -46,7 +46,8 @@ Optyn::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile |= Dir["#{Rails.root}/app/assets/stylesheets/back_end_static_page/*.*"]
+  config.assets.precompile |= Dir["#{Rails.root}/app/assets/stylesheets/front_end_static_page/*.*"]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
