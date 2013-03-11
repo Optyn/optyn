b3e42dda7e7fb54835866213340d6cfb1b58689e
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-03-11 09:28:10 +0530

    [45919083] Removing the commented code. Some reordering more changes to come.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Optyn::Application.routes.draw do
 
-  use_doorkeeper do
-    controllers :authorizations => 'oauth_authorizations'
-  end
-
   root to: 'main#index'
 
   # Static Pages created by Alen
@@ -85,66 +81,14 @@ namespace "merchants" do |merchant|
     end
 
     resource :app 
-  end
+end
+
+use_doorkeeper do
+    controllers :authorizations => 'oauth_authorizations'
+end
 
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
-
+  #Link to the customer facing side static pages converted to layout with HAML
   namespace "front_end_static_page" do |front|
     resources :samples do
       collection do
@@ -163,6 +107,7 @@ namespace "merchants" do |merchant|
     end
   end
 
+  #Link to the admin side static pages converted to layout with HAML
   namespace "back_end_static_page" do |back|
     resources :samples do
       collection do
