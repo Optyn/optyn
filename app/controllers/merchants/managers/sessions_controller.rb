5d95fa147febc5618639d85d6038b67aaae05282
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-03-12 18:42:31 +0530

    Don't go to the managers screen when the consumers is signed in

--- app/controllers/merchants/managers/sessions_controller.rb
+++ app/controllers/merchants/managers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Merchants::Managers::SessionsController < Devise::SessionsController
-  before_filter :require_no_manager
+  before_filter :require_no_manager, except: [:destroy]
+
   include MerchantSessionsRedirector
 
   def create
@@ -7,5 +8,10 @@ class Merchants::Managers::SessionsController < Devise::SessionsController
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
     after_sign_in_path_for(resource)
+  end
+
+  def destroy
+  	super
+  	reset_session
   end
 end
