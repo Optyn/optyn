3d233613fde6eed91d8ad26de872371b9fa0898c
deepali <deepali@idyllic-software.com>
2013-03-19 12:48:49 +0530

    [46278313]Shop show page added & bug fixed

--- app/controllers/merchants/shops_controller.rb
+++ app/controllers/merchants/shops_controller.rb
@@ -11,10 +11,14 @@ class Merchants::ShopsController < Merchants::BaseController
 		@shop = current_shop
 		if @shop.update_attributes(params[:shop])
 			flash[:notice] = "Shop details updated successfully"
-      redirect_to root_path
+      redirect_to merchants_shop_path
     else
       @shop_errors = @shop.errors
       render :action => :edit
     end
+	end
+	
+	def show
+		@shop = current_shop
 	end
 end
