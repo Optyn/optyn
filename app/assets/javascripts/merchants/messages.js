8f6e94dda76aba1728c5d64825eb0d49adbae517
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-04-02 11:13:06 +0530

    Remove the send_on attr and send_immediately options common to all campaign templates

--- app/assets/javascripts/merchants/messages.js
+++ app/assets/javascripts/merchants/messages.js
@@ -6,21 +6,6 @@ $(document).ready(function () {
 function MerchantMessage() {
     this.initialize = function () {
         if ($('#message_fields_wrapper').length) {
-            this.hookWysihtml5();
-            this.hookDatetimepicker();
         }
-    };
-
-    this.hookWysihtml5 = function () {
-        $('#message_fields_wrapper .details').wysihtml5();
-    };
-
-    this.hookDatetimepicker = function(){
-      $('#message_send_on_wrapper').datetimepicker({
-          language: 'en',
-          pick12HourFormat: true,
-          pickSeconds: false,
-          startDate: new Date()
-      });
     };
 }
