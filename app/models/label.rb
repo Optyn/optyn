4d927538a6119adddb8b8e10ce7da23148cdcf66
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-04-03 13:08:55 +0530

    Adding active flag to the Label, making it a default scope and adding helper methods with exclusive scopes

--- app/models/label.rb
+++ app/models/label.rb
@@ -4,7 +4,26 @@ class Label < ActiveRecord::Base
 
   attr_accessible :shop_id, :name
 
+  default_scope where(active: true)
+
+  SELECT_ALL_NAME = 'Select All'
+
+  def self.active_and_inactive(shop)
+    with_exclusive_scope { shop.labels.all }
+  end
+
+  def self.inactive(shop)
+    with_exclusive_scope { shop.labels.where(active: false) }
+  end
+
   def users_count
     user_labels.count
+  end
+
+  def inactive?
+    !active
+  end
+  def select_all?
+    inactive? && SELECT_ALL_NAME == name
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -13,39 +13,39 @@ class Shop < ActiveRecord::Base
   has_many :interests, :as => :holder
   has_many :labels, dependent: :destroy
 
-  SHOP_TYPES=['local','online']
-  attr_accessible :name,:stype,:managers_attributes,:locations_attributes,:description, :logo_img, :business_ids, :website, :identifier, :time_zone
+  SHOP_TYPES=['local', 'online']
+  attr_accessible :name, :stype, :managers_attributes, :locations_attributes, :description, :logo_img, :business_ids, :website, :identifier, :time_zone
   mount_uploader :logo_img, ImageUploader
 
-  validates :name,:presence=>true
-  validates :stype, :presence => true, :inclusion => { :in => SHOP_TYPES , :message => "is Invalid" }
+  validates :name, :presence => true
+  validates :stype, :presence => true, :inclusion => {:in => SHOP_TYPES, :message => "is Invalid"}
   validates :identifier, uniqueness: true, presence: true, unless: :new_record?
   validates :time_zone, presence: true, unless: :new_record?
 
   accepts_nested_attributes_for :managers
-  accepts_nested_attributes_for :locations 
+  accepts_nested_attributes_for :locations
 
   scope :includes_app, includes(:oauth_application)
 
   scope :inlcudes_locations, includes(:locations)
-  
-  scope :for_app_id, ->(app_id){inlcudes_locations.includes_app.where(["oauth_applications.uid = :app_id", {app_id: app_id}])}
 
-  scope :fetch_same_identifier, ->(shop_id, q){where(["shops.id <> :shop_id AND shops.identifier LIKE :q", {shop_id: shop_id, q: q}])}
+  scope :for_app_id, ->(app_id) { inlcudes_locations.includes_app.where(["oauth_applications.uid = :app_id", {app_id: app_id}]) }
 
-  after_create :create_dummy_survey
+  scope :fetch_same_identifier, ->(shop_id, q) { where(["shops.id <> :shop_id AND shops.identifier LIKE :q", {shop_id: shop_id, q: q}]) }
+
+  after_create :create_dummy_survey, :create_select_all_label
   #INDUSTRIES = YAML.load_file(File.join(Rails.root,'config','industries.yml')).split(',')
 
   def self.by_app_id(app_id)
-    for_app_id(app_id).first  
+    for_app_id(app_id).first
   end
 
   def shop_already_exists?
-    Shop.where("name LIKE ?",self.name).count != 0
+    Shop.where("name LIKE ?", self.name).count != 0
   end
 
   def update_manager
-    self.managers.first.update_attributes(:owner=>true)
+    self.managers.first.update_attributes(:owner => true)
   end
 
   def get_shop_owner
@@ -70,11 +70,11 @@ class Shop < ActiveRecord::Base
 
   def generate_oauth_token(redirect_uri, force=false)
     app = nil
-    if force 
+    if force
       oauth_application.destroy
       reload
     end
-    
+
     app = self.oauth_application
     if app.present?
       app.redirect_uri = redirect_uri
@@ -85,14 +85,14 @@ class Shop < ActiveRecord::Base
   end
 
   def api_welcome_details
-    details  = as_json(only: "name")
+    details = as_json(only: "name")
     details[:user_count] = users.count
 
     details[:location] = {}
 
     useful_location = first_location
     if useful_location.present?
-      details[:location] = useful_location.as_json(except: [:id, :created_at, :updated_at, :longitude, :latitude]) 
+      details[:location] = useful_location.as_json(except: [:id, :created_at, :updated_at, :longitude, :latitude])
     end
 
     details[:button_url] = SiteConfig.app_base_url + "/assets/logo.png"
@@ -132,10 +132,16 @@ class Shop < ActiveRecord::Base
     end
   end
 
+  def create_select_all_label
+    label = Label.new(shop_id: self.id,  name: Label::SELECT_ALL_NAME)
+    label.active = false
+    label.save
+  end
+
   def generate_application(redirect_uri)
     Shop.transaction do
-      app = Doorkeeper::Application.new(:name => self.name + self.first_location_zip, 
-        :redirect_uri => redirect_uri)
+      app = Doorkeeper::Application.new(:name => self.name + self.first_location_zip,
+                                        :redirect_uri => redirect_uri)
       app.owner = self
       app.save
 
