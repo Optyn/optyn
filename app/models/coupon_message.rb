6e99d3753fb95de761215e6e6da8548d4db7f3e3
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-04-08 16:52:29 +0530

    Fix the coupon messages

--- app/models/coupon_message.rb
+++ app/models/coupon_message.rb
@@ -1,6 +1,8 @@
 class CouponMessage < Message
-  attr_accessible :type_of_discount, :discount_amount, :fine_print
+  attr_accessible :type_of_discount, :discount_amount, :fine_print, :coupon_code
 
   validates :content, presence: true
   validate :validate_beginning
+  validates :type_of_discount, presence: true
+  validates :discount_amount, presence: true
 end
