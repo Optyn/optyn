41fc808aecb776d28363482d95583d9be42ea3d9
Chetan <chetan@idyllic-software.com>
2013-04-08 18:34:50 +0530

    Redirect to account page when email is not present

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,7 +1,12 @@
 class BaseController < ApplicationController
   #Please don't write any actions here. This class is storing the common behavior for a logged in user.
   before_filter(:authenticate_user!)
-
+  before_filter :redirect_to_account
+  
   private
-
+  def redirect_to_account
+		if !current_user.blank? && !current_user.email.present?
+			redirect_to edit_user_registration_path
+		end
+  end
 end
