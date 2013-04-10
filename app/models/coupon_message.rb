8efad6250dc0431523dca6f530016a439f6af970
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-04-10 10:27:45 +0530

    Adding appropirate validation for discount amount field

--- app/models/coupon_message.rb
+++ app/models/coupon_message.rb
@@ -1,8 +1,20 @@
 class CouponMessage < Message
   attr_accessible :type_of_discount, :discount_amount, :fine_print, :coupon_code, :content
 
-  validate :validate_beginning
+  validate :validate_ending
   validates :type_of_discount, presence: true
-  validates :discount_amount, presence: true
+  validate :validate_discount_amount
   validates :content, presence: true
+
+  private
+  def validate_discount_amount
+    return self.errors.add(:base, "You need to add the discount amount") if discount_amount.blank?
+    numeric_amount = discount_amount.to_i
+
+    if percentage_off?
+      self.errors.add(:base, "Please add valid values between 0 - 100") if numeric_amount <= 0 || numeric_amount > 100
+    else
+      self.errors.add(:base, "Please make sure you add a numeric value") if numeric_amount <= 0
+    end
+  end
 end
