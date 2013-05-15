ea050091cd65eef60afeda26de24982becbb8157
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-05-15 17:23:46 +0530

    Cached dashboard values for connections for dashboards

--- app/controllers/merchants/dashboard_controller.rb
+++ app/controllers/merchants/dashboard_controller.rb
@@ -1,3 +1,9 @@
 class Merchants::DashboardController < Merchants::BaseController
-
+  def index
+    shop_id = current_shop.id
+    @total_count = Connection.shop_connections_count_total(shop_id)
+    @month_count = Connection.shop_connections_count_month(shop_id)
+    @week_count = Connection.shop_connections_count_week(shop_id)
+    @day_count = Connection.shop_connections_count_day(shop_id)
+  end
 end
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,12 +1,12 @@
 class Connection < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :shop
+  belongs_to :user
+  belongs_to :shop
 
-	attr_accessible :user_id, :shop_id, :active, :connected_via
+  attr_accessible :user_id, :shop_id, :active, :connected_via
 
   scope :active, where(active: true)
 
-	scope :for_shop, ->(shop_identifier){where(shop_id: shop_identifier)}
+  scope :for_shop, ->(shop_identifier) { where(shop_id: shop_identifier) }
 
   scope :includes_business_and_locations, includes(shop: [:locations, :businesses])
 
@@ -14,22 +14,59 @@ class Connection < ActiveRecord::Base
 
   scope :includes_user_and_permissions, includes(user: {permissions_users: :permission})
 
-  scope :for_users, ->(user_identifiers){where(user_id: user_identifiers)}
+  scope :for_users, ->(user_identifiers) { where(user_id: user_identifiers) }
 
-  def toggle_connection
-  	if self.active
-  		self.active = false
-  	else
-  		self.active = true
-  	end	
-  	self.save
-  end
+  scope :time_range, ->(start_timestamp, end_timestamp) { where(created_at: start_timestamp..end_timestamp) }
 
-  def connection_status
-  	self.active ? "Following" : "Opt in"
-  end
+  scope :for_shop_in_time_range, ->(shop_id, start_timestamp, end_timestamp) { for_shop(shop_id).time_range(start_timestamp, end_timestamp) }
 
   def self.shops_connections(shop_id)
     for_shop(shop_id).includes_user_and_permissions
+  end
+
+  def self.shop_connections_count_total(shop_id, force = false)
+    cache_key = create_count_cache_key(shop_id, "total")
+    Rails.cache.fetch(cache_key, :force => force, :expires_in => SiteConfig.ttls.merchant_dashbaord_count) do
+      for_shop(shop_id).count
+    end
+  end
+
+  def self.shop_connections_count_month(shop_id, force = false)
+    cache_key = create_count_cache_key(shop_id, "month")
+    Rails.cache.fetch(cache_key, :force => force, :expires_in => SiteConfig.ttls.merchant_dashbaord_count) do
+      for_shop_in_time_range(shop_id, Time.now.beginning_of_month.beginning_of_day, Time.now.end_of_day).count
+    end
+  end
+
+  def self.shop_connections_count_week(shop_id, force = false)
+    cache_key = create_count_cache_key(shop_id, "week")
+    Rails.cache.fetch(cache_key, :force => force, :expires_in => SiteConfig.ttls.merchant_dashbaord_count) do
+      for_shop_in_time_range(shop_id, Time.now.beginning_of_week.beginning_of_day, Time.now.end_of_day).count
+    end
+  end
+
+  def self.shop_connections_count_day(shop_id, force = false)
+    cache_key = create_count_cache_key(shop_id, "day")
+    Rails.cache.fetch(cache_key, :force => force, :expires_in => SiteConfig.ttls.merchant_dashbaord_count) do
+      for_shop_in_time_range(shop_id, Date.yesterday.beginning_of_day, Time.now.end_of_day).count
+    end
+  end
+
+  def toggle_connection
+    if self.active
+      self.active = false
+    else
+      self.active = true
+    end
+    self.save
+  end
+
+  def connection_status
+    self.active ? "Following" : "Opt in"
+  end
+
+  private
+  def self.create_count_cache_key(shop_id, suffix)
+    "connection-count-shop-#{shop_id}-time-range-#{suffix}"
   end
 end
