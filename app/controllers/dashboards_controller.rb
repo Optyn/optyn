a2a173593d16ccc7a9fa71e41af7351403fad43e
Manjiri <patmanjiri@gmail.com>
2013-06-03 17:14:49 +0530

    Fix for a small exception in dashboard feed

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -92,7 +92,7 @@ class DashboardsController < BaseController
               shop_name: connection.shop.name,
               image_url: connection.shop.logo_img.url,
               excerpt: connection.shop.description,
-              action_url: shop_connection_path(connection.shop.identifier)
+              action_url: (shop_connection_path(connection.shop.identifier)rescue '')
           }
       )
     end
