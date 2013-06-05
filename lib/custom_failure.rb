3b15b95e343fb7f9588d8956da6c7ce6daa1ea59
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-06-05 16:41:52 -0500

    Fix when the email used is not present anywhere

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -4,7 +4,10 @@ class CustomFailure < Devise::FailureApp
       api_login_path(redirect_uri: params[:redirect_uri])
     elsif warden_options[:scope] == :user || warden_options[:scope] == :merchants_manager
 
-      if (params[:user].present? || params[:merchants_manager].present?) && (instance = (User.find_by_email(params[:user][:email]) || Manager.find_by_email(params[:merchants_manager][:email]))).present?  && instance.authentications.present?
+      if (params[:user].present? || params[:merchants_manager].present?) &&
+          (instance = (User.find_by_email(params[:user][:email]) || Manager.find_by_email(params[:merchants_manager][:email]) rescue nil)).present?  &&
+          instance.authentications.present?
+
         provider = instance.authentications.last.provider.gsub(/_oauth2/, '').humanize
         flash[:alert] = "We have found that you are registered with #{provider} in Optyn. Please login appropriately."
       else
