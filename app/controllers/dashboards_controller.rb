5e64bba8fdcfa1ece3483fee5d87e364c7d500a2
Manjiri <patmanjiri@gmail.com>
2013-06-05 13:38:31 +0530

    Reverted customer dashboard links

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -47,7 +47,7 @@ class DashboardsController < BaseController
               shop_name: connection.shop.name,
               image_url: connection.shop.logo_img.url,
               excerpt: connection.shop.description,
-              action_url: connections_path
+              action_url: (shop_connection_path(connection.shop.identifier) rescue '')
           }
       )
     end
@@ -92,7 +92,7 @@ class DashboardsController < BaseController
               shop_name: connection.shop.name,
               image_url: connection.shop.logo_img.url,
               excerpt: connection.shop.description,
-              action_url: connections_path
+              action_url: (shop_connection_path(connection.shop.identifier) rescue '')
           }
       )
     end
