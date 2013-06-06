80cfaddbd7acae86b80487af6bb832618e91e239
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-06-06 16:44:56 -0500

    Pre-Launch stuff deleted

--- app/mailers/pre_launch_mailer.rb
+++
@@ -1,8 +0,0 @@
-class PreLaunchMailer < ActionMailer::Base
-  default from: "Optyn.com <services@optyn.com>"
-
-  def welcome(registration)
-  	@registration = registration
-  	mail(:to => registration.email)
-  end
-end
