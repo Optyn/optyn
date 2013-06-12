9bdca884d293e293d8a9da77051ccc217e52e974
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-06-12 13:18:41 -0500

    change the verbiage of zip error code

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,7 +9,7 @@ class Location < ActiveRecord::Base
   validates :city, :presence=>true
   validates :street_address1, :presence=>true
   validates :zip, :presence=>true
-  validates :zip,:length=>{:minimum=> 5,:maximum=>5,:message=>"code is invalid"},:if => lambda{|location| location.zip.length!=0}
+  validates :zip,:length=>{:minimum=> 5,:maximum=>5,:message => "is invalid"},:if => lambda{|location| location.zip.length!=0}
   validates_format_of :zip, :with => /^\d+$/,:if => lambda{|location| location.zip.length!=0}
   validates :state_id, :presence => true
   #validates :longitude, :presence=>true
