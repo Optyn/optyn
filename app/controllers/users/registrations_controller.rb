50fc73d616ef404b3100abe1c44a8d425a4baa61
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-06-19 14:29:36 -0500

    Add capability to add zip codes on the profile page. Fix the saving of birth_date and gender

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :require_manager_logged_out
   before_filter :redirect_to_account, only: [:profile, :update_profile]
-  
 
 
   def new
-  	session[:omniauth_manager] =nil
+    session[:omniauth_manager] =nil
     session[:omniauth_user] =true
     super
   end
@@ -34,17 +33,30 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  
+
   def profile
-  	@user = current_user
+    @user = current_user
     @permissions_user = @user.permissions_users.present? ? @user.permissions_users : @user.build_permission_users
   end
 
   def update_profile
-  	@user = current_user
-    if @user.update_attributes(params[:user])
-      flash[:notice] = "Profile updated successfully"
-      redirect_to connections_path
+    @user = current_user
+    @user.attributes = params[:user].except(:birth_date)
+
+    if params[:user][:birth_date].present?
+      begin
+        @user.birth_date = Time.strptime(params[:user][:birth_date], '%m/%d/%Y')
+      rescue
+        @user.errors[:birth_date] = "invalid"
+      end
+    else
+      @user.birth_date = nil
+    end
+
+    if @user.save
+      @permissions_user = @user.permissions_users.present? ? @user.permissions_users : @user.build_permission_users
+      flash.now[:notice] = "Profile updated successfully"
+      render action: "profile"
     else
       render 'edit'
     end
