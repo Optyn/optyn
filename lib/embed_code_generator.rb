9fc19f0bc3e40a4ca1118e250d62546b747fdf55
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-07-07 21:22:32 -0500

    Bug fix

--- lib/embed_code_generator.rb
+++ lib/embed_code_generator.rb
@@ -1,7 +1,7 @@
 class EmbedCodeGenerator
 	def self.generate_embed_code(app)
     %Q(
-      <script type="text/javascript" src="//#{SiteConfig.app_base_url}/api/shop/button_script.js?app_id=#{app.uid}"></script>
+      <script type="text/javascript" src="#{SiteConfig.app_base_url}/api/shop/button_script.js?app_id=#{app.uid}"></script>
     )
 	end
 end
