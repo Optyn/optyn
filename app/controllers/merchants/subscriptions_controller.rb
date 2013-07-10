aa6ce7c53a95777a94c2735e097c6ec5f5c9446f
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-07-10 12:47:06 -0500

    Bug fixes subscription

--- app/controllers/merchants/subscriptions_controller.rb
+++ app/controllers/merchants/subscriptions_controller.rb
@@ -1,22 +1,23 @@
 class Merchants::SubscriptionsController < Merchants::BaseController
-  
+
   before_filter :require_manager
   before_filter :require_shop_local_and_inactive, :only => [:upgrade, :subscribe]
   skip_before_filter :active_subscription?, :only => [:upgrade, :subscribe]
+
   def upgrade
-    @plan=Plan.starter
+    @plan = current_shop.subscription.plan
     @subscription=current_merchants_manager.shop.subscription || @plan.subscriptions.build
     flash[:notice] = 'You will be charged based on the number of connections. For details, refer our pricing plans'
   end
 
   def edit_billing_info
-    @plan=current_merchants_manager.shop.subscription.plan
-    @subscription = current_merchants_manager.shop.subscription
+    @plan= current_shop.subscription.plan
+    @subscription = current_shop.subscription
   end
 
   def update_billing_info
-    @subscription = current_merchants_manager.shop.subscription
-    
+    @subscription = current_shop.subscription
+
     begin
       @stripe_customer= Stripe::Customer.retrieve(@subscription.stripe_customer_token)
       @stripe_customer.card = params['stripeToken']
@@ -29,14 +30,19 @@ class Merchants::SubscriptionsController < Merchants::BaseController
   end
 
   def subscribe
-    begin 
-      @plan=Plan.starter
-      @subscription=Subscription.new(params[:subscription])
-      customer = Subscription.create_stripe_customer(params)
-      @subscription.stripe_customer_token=customer.id
+    begin
+      @plan = current_shop.subscription.plan
+      params[:subscription][:plan_id] = @plan.plan_id
+      params[:stripe_plan_id] = @plan.id
+      @subscription = current_shop.subscription || Subscription.new()
+      @subscription.attributes = params[:subscription]
+      if @subscription.stripe_customer_token.blank?
+        customer = Subscription.create_stripe_customer(params)
+        @subscription.stripe_customer_token = customer.id
+      end
 
       if @subscription.save
-        @subscription.update_attribute(:active, true)
+        @subscription.update_attribute(:active, true) if @subscription.stripe_customer_token.present?
         amount = customer.subscription.plan.amount
         conn_count = current_shop.active_connection_count
         last4 = customer.active_card.last4
