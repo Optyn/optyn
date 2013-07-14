7b94ca2f2dc327b4b6de186cc7c64da569bb2618
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-07-13 19:00:29 -0500

    Don't resque from stripe event handlers

--- lib/stripe_event_handlers.rb
+++ lib/stripe_event_handlers.rb
@@ -3,36 +3,26 @@ module StripeEventHandlers
   def self.handle_customer_subscription_created(params)
     @subscription=Subscription.find_by_stripe_customer_token(params['data']['object']['customer'])
     @subscription.update_attributes(:active => true)
-  rescue => e
-    Rails.logger.error e
   end
 
   def self.handle_customer_subscription_updated(params)
     @subscription = Subscription.find_by_stripe_customer_token(params['data']['object']['customer'])
     status = params['data']['object']['customer']['subscription']['status']
     @subscription.update_attributes(:active => ((status == 'active' || status == 'trialing') ? true : false))
-  rescue => e
-    Rails.logger.error e
   end
 
   def self.handle_customer_subscription_deleted(params)
     @subscription=Subscription.find_by_stripe_customer_token(params['data']['object']['customer'])
     @subscription.update_attributes(:active => false) if @subscription
-  rescue => e
-    Rails.logger.error e
   end
 
   def self.handle_plan_deleted(params)
     @plan=Plan.find_by_plan_id(params['data']['object']['id'])
     @plan.destroy if @plan
-  rescue => e
-    Rails.logger.error e
   end
 
   def self.handle_plan_created(params)
-    begin
       @stripe_plan = Stripe::Plan.retrieve(params['data']['object']['id'])
-
       @plan = Plan.find_by_plan_id(@stripe_plan.id) || Plan.new
       @plan.plan_id = @stripe_plan.id
       @plan.name = @stripe_plan.name
@@ -40,14 +30,9 @@ module StripeEventHandlers
       @plan.amount = @stripe_plan.amount
       @plan.currency = @stripe_plan.currency
       @plan.save!
-
-    rescue => e
-      Rails.logger.error e
-    end
   end
 
   def self.handle_plan_updated(params)
-    begin
       @stripe_plan = Stripe::Plan.retrieve(params['data']['object']['id'])
       @plan=Plan.find_by_plan_id(params['data']['object']['id']) if @stripe_plan
       @plan.update_attributes(:plan_id => @stripe_plan.id,
@@ -55,9 +40,6 @@ module StripeEventHandlers
                               :interval => @stripe_plan.interval,
                               :amount => @stripe_plan.amount,
                               :currency => @stripe_plan.currency)
-    rescue => e
-      Rails.logger.error e
-    end
   end
 
   def self.handle_invoice_created(params)
@@ -65,8 +47,6 @@ module StripeEventHandlers
     evaluated_plan = Plan.which(subscription.shop)
     subscription.update_plan(evaluated_plan) if evaluated_plan != subscription.plan
     ShopAudit.create(shop_id: subscription.shop.id, description: "Test Invoice Created")
-  rescue => e
-    Rails.logger.error e
   end
 
   def self.handle_invoice_payment_succeeded(params)
@@ -74,8 +54,6 @@ module StripeEventHandlers
     amount = params['data']['object']['total']
     conn_count = subscription.shop.active_connection_count
     Resque.enqueue(PaymentNotificationSender, "MerchantMailer", "invoice_payment_succeeded", {shop_id: subscription.shop.id, connection_count: conn_count, amount: amount})
-  rescue => e
-    Rails.logger.error e
   end
 
   def self.handle_invoice_payment_failed(params)
@@ -84,8 +62,6 @@ module StripeEventHandlers
     amount = params['data']['object']['total']
     conn_count = subscription.shop.active_connection_count
     Resque.enqueue(PaymentNotificationSender, 'MerchantMailer', 'invoice_payment_failure', {shop_id: subscription.shop.id, amount: amount, connection_count: conn_count})
-  rescue => e
-    Rails.logger.error e
   end
 
   def self.handle_invoice_updated(params)
@@ -99,8 +75,6 @@ module StripeEventHandlers
           :amount => params['data']['object']['total']
       )
     end
-  rescue => e
-    Rails.logger.error e
   end
 
   def self.handle_coupon_created(params)
