8885379449cd31d3a5bb0dcb2696ac726a08e192
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-08-08 13:18:12 -0500

    Bug Fix reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,13 @@ require "#{File.dirname(__FILE__)}/seed_data/plans.rb"
 require "#{File.dirname(__FILE__)}/seed_data/states.rb"
 require "#{File.dirname(__FILE__)}/seed_data/permissions.rb"
 require "#{File.dirname(__FILE__)}/seed_data/message_folders.rb"
+puts "Adding the Optyn Partner"
 Rake::Task['partner:create_optyn'].invoke
+puts "Generating client_id and client_secret for Optyn Magic Owner"
 Rake::Task['oauth2:generate_optyn_magic_application'].invoke
+puts "Adding Virtual Chrome Extension Shop"
 Rake::Task['shop:add_browser_extensions'].invoke
+puts "Adding the shop for Optyn Magic"
 Rake::Task['shop:add_optyn_magic_shop'].invoke
 
 #Add some dummy data in the development mode.
