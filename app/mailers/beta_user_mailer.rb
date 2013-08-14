b3adbb0173efe638ca67ae5a5dde34b844eb5164
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-08-13 21:25:06 -0500

    Fixing the from syntax

--- app/mailers/beta_user_mailer.rb
+++ app/mailers/beta_user_mailer.rb
@@ -1,5 +1,5 @@
 class BetaUserMailer < ActionMailer::Base
-	default from: '"Optyn.com" <email@optyn.com>'
+	default from: 'Optyn.com <email@optyn.com>'
 
 	def send_invite(email_address)
 		mail(:to => email_address, :subject => "Optyn is now live. Check it out!" )
