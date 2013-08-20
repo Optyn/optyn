0a5be77849bcfb672a06b393774e32930b322031
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2013-08-20 14:02:27 +0530

    Updates to Optyn Button. More layout tweaks for merchants's app.

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,5 @@
 class ShopsController < ApplicationController
+  layout 'flat'
   def show
     @shop  = Shop.find_by_identifier(params[:identifier])
     redirect_to(new_user_session_path, notice: "Could not find the shop you are looking for.")  if @shop.blank?
