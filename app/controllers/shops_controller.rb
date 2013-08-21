a58e1e13fdb108e73745f008ad41dacb64520fbf
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2013-08-21 14:43:47 +0530

    Changes to shops controller.

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,4 @@
 class ShopsController < ApplicationController
-  layout 'flat'
   def show
     @shop  = Shop.find_by_identifier(params[:identifier])
     redirect_to(new_user_session_path, notice: "Could not find the shop you are looking for.")  if @shop.blank?
