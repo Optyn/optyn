4665114b6c84c9a1d40212f4124e31062141bd32
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-08-23 08:48:38 -0500

    Spell fix method call

--- app/workers/ses_email_sender.rb
+++ app/workers/ses_email_sender.rb
@@ -7,7 +7,7 @@ class SesEmailSender
     message = message_user.message
 
     ses_message = MessageMailer.send_announcement(message, message_user).deliver
-    message_email_auditor.udpate_attribute(:delivered, true)
+    message_email_auditor.update_attribute(:delivered, true)
     message_email_auditor.update_attributes(ses_message_id: ses_message['message_id'].to_s.split(/@/).first)
   end
 end
