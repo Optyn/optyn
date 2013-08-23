5afdb604a39cf7a151e71979ebdfac2182f58a13
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-08-23 13:46:01 -0500

    Fix the password reset redirection and errors

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -13,6 +13,20 @@ class Users::PasswordsController < Devise::PasswordsController
     end
   end
 
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+      set_flash_message(:notice, flash_message) if is_navigational_format?
+      sign_in(resource_name, resource)
+      redirect_to(consumers_root_path)
+    else
+      render action: 'edit'
+    end
+  end
+
   private
   def populate_user_and_manager
     @user = User.find_by_email(resource_params[:email])
