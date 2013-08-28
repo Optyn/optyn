15e69fd5509ef37e54507dad0a20477292caccc6
shashank singh <shashank.personal@gmail.com>
2013-08-28 18:57:35 +0530

    added doorkepper to shopscontroller

--- app/controllers/api/v1/merchants/shops_controller.rb
+++ app/controllers/api/v1/merchants/shops_controller.rb
@@ -2,8 +2,8 @@ module Api
   module V1
     module Merchants
       class ShopsController < PartnerOwnerBaseController
-	#doorkeeper_for :all
-
+	doorkeeper_for :all
+	before_filter :import_list, :import, :import_status, :update
         def import_list
           partner_id = current_partner.id
           @import_list = ApiRequestPayload.for_partner(partner_id)
