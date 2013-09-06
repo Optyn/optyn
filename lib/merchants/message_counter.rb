6d18ab864d4d1e41981078c3a0254138ed8873ca
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-09-06 14:12:13 +0530

    Show messages for a shop but manager campaigncenter

--- lib/merchants/message_counter.rb
+++ lib/merchants/message_counter.rb
@@ -5,8 +5,8 @@ module Merchants
    end
 
    def populate_folder_count
-     @drafts_count = Message.cached_drafts_count(current_manager)
-     @queued_count = Message.cached_queued_count(current_manager)
+     @drafts_count = Message.cached_drafts_count(current_shop)
+     @queued_count = Message.cached_queued_count(current_shop)
    end
  end
 end
