f11d9aeeadddc31ad51742c86f22d4170c24ca17
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-09-09 12:13:31 +0530

    RFC 3696 compliance for sender and receiver

--- app/models/message.rb
+++ app/models/message.rb
@@ -516,7 +516,7 @@ class Message < ActiveRecord::Base
 
   def canned_from
     #manager.email_like_from
-    %{#{self.shop_name.titleize} <email@optyn.com>}
+    %{"#{self.shop_name.titleize}" <email@optyn.com>}
   end
 
   def canned_subject
