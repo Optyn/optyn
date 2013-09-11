aebfc8fa73b3c7eb2b709f8fe128daf73150b466
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2013-09-11 14:56:44 +0530

    Developed new merchants only homepage.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,23 +1,4 @@
 class MainController < ApplicationController
   before_filter :require_not_logged_in, only: [:index]
 
-  # def index
-  #   render layout: 'flat'
-  # end
-
-  # def terms
-  #   render layout: 'flat'
-  # end
-
-  # def about
-  #   render layout: 'flat'
-  # end
-
-  # def faq
-  #   render layout: 'flat'
-  # end
-
-  # def consumerfeatures
-  #   render layout: 'flat'
-  # end
 end
