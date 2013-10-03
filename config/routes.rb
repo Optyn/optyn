be345df9fa99479eef80e3016094690d20d29616
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-10-03 21:34:29 +0530

    Bug Fix: Revert the managers signup route change

--- config/routes.rb
+++ config/routes.rb
@@ -140,22 +140,12 @@ Optyn::Application.routes.draw do
 
   namespace "merchants" do |merchant|
 
-    devise_for :managers, :path => '', :path_names => {:sign_in => 'signin',
-                                     :sign_up => 'signup'
-  }, :controllers => {
+    devise_for :managers, :controllers => {
         :registrations => 'merchants/managers/registrations',
         :sessions => 'merchants/managers/sessions',
         :passwords => 'merchants/managers/passwords',
         :confirmations => 'merchants/managers/confirmations'
     }
-
-    devise_scope :managers do
-      post '/signup' => 'managers/registrations#create', :as => :manager_registration
-      get '/signup' => 'managers/registrations#new', as: :new_manager_registration
-      get "show_managers" => "merchant_managers#show_managers", as: :managers_list
-      get '/add_manager' => 'merchant_managers#add_manager'
-      post '/create_new_manager' => 'merchant_managers#create_new_manager'
-    end
 
     resource :app
     resources :connections
