997e140760b2994a235a51b83b6df07e82757730
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-10-11 15:29:08 +0530

    Knock off the white-label-stging stage

--- config/deploy/white-label-staging.rb
+++
@@ -1,4 +0,0 @@
-server "ec2-54-211-197-135.compute-1.amazonaws.com", :web, :app, :db, :messenger, :resque_worker, :resque_schedular, primary: true
-set :branch, "white-label-development"
-set :rails_env, 'staging'
-set :local_app_url, 'http://localhost:3000/'
