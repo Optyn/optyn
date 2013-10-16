06ca9c861d4d4a18cde2e500a31230a8f26543f1
Shashank Singh <shashank.personal@gmail.com>
2013-10-16 20:39:45 +0530

    fixing worker

--- lib/users/shop_user_importer.rb
+++ lib/users/shop_user_importer.rb
@@ -1,33 +1,32 @@
-module Shops
-  module Importer  
+module Users
+  module ShopUserImporter  
     def user_import(payload)
       binding.pry
-#       content = download_csv_file(payload)
+      content = download_csv_file(payload)
 
-#       csv_table = CSV.parse(content, { headers: true, converters: :numeric, header_converters: :symbol})
-#       headers = csv_table.headers
-#       validate_headers(headers)
-#       binding.pry
+      csv_table = CSV.parse(content, { headers: true, converters: :numeric, header_converters: :symbol})
+      headers = csv_table.headers
+      validate_headers(headers)
 
-#       output = []
-#       unparsed_rows = []
-#       output_headers = %{"Shop","Email","Name",Gender","Birth Date"}
-#       output << output_headers
-#       unparsed_rows << output_headers
+      output = []
+      unparsed_rows = []
+      output_headers = %{"Shop","Email","Name",Gender","Birth Date"}
+      output << output_headers
+      unparsed_rows << output_headers
       
-#       counters = ActiveSupport::OrderedHash.new()
-#       counters[:users_created] = 0
-#       counters[:existing_users] = 0
-#       counters[:unparsed_rows] = 0
+      counters = ActiveSupport::OrderedHash.new()
+      counters[:users_created] = 0
+      counters[:existing_users] = 0
+      counters[:unparsed_rows] = 0
 
-#       csv_table.each do |row|
-#         status = nil
-#         output_row = [%{"#{row[:shop]}"}, %{"#{row[:email]}"}, %{"#{row[:name]}"}, %{"#{row[:gender]}"}, %{"#{row[:birth_date]}"}]
+      csv_table.each do |row|
+        status = nil
+        output_row = [%{"#{row[:shop]}"}, %{"#{row[:email]}"}, %{"#{row[:name]}"}, %{"#{row[:gender]}"}, %{"#{row[:birth_date]}"}]
         
-#         begin
-#           shop_name = row[:shop]
-#           Shop.transaction do
-#             shop = for_name(shop_name)
+        begin
+          shop_name = row[:shop]
+          Shop.transaction do
+            shop = for_name(shop_name)
 #             # if shop.present?
 #             #   # user = User.find_or_initialize_by_email(row[:email])	
 #             #   # user.name = row[:name]
@@ -35,7 +34,7 @@ module Shops
 #             #   shop_id = 
 #             #   user.shop_id = shop_id
 #             # end
-# 		      user = User.find_by_email(row[:email]) || User.new(email: row[:email])
+		      user = User.find_by_email(row[:email]) || User.new(email: row[:email])
 # 		      user.skip_name = true
 # 		      user.skip_welcome_email = true
 # 		      user.name = row[:name] unless user.name.present?
@@ -70,7 +69,7 @@ module Shops
 # 		        counters[:existing_user] += 1
 # 		        output_row << %{"User exists"}
 # 		      end
-# 		      user.save()
+		      user.save()
 
 
 #             #   status = %{"New User"}
@@ -83,15 +82,15 @@ module Shops
 #             #   counters[:existing_user] += 1 
 #             #   output << output_row.join(",")
 #             # end
-#           #end
-#         # rescue Exception => e    
-#         #   Rails.logger.error e.message
-#         #   Rails.logger.error e.backtrace
-#         #   counters[:unparsed_rows] += 1
-#         #   status = %{"Error: #{e.message}"}
-#         #   output_row << status
-#         #   unparsed_rows << output_row.join(",")
-#         # end
+          end#end of transaction do
+        # rescue Exception => e    
+        #   Rails.logger.error e.message
+        #   Rails.logger.error e.backtrace
+        #   counters[:unparsed_rows] += 1
+        #   status = %{"Error: #{e.message}"}
+        #   output_row << status
+        #   unparsed_rows << output_row.join(",")
+        end
 #         output << output_row.join(",")
 #       end
 
@@ -100,19 +99,19 @@ module Shops
 #       [[counters], output.join("\n"), unparsed]
     end#end of user_import
 
-#     def validate_headers(headers)
-#       # binding.pry
-#       if !headers.include?(:shop) || !headers.include?(:name) || !headers.include?(:gender) || !headers.include?(:birth_date)
-#         raise "Incorrect Headers. The file should have headers of 'Shop','Name','Gender', 'Birth Date'" 
-#       end  
-#     end
+    def validate_headers(headers)
+      # binding.pry
+      if !headers.include?(:shop) || !headers.include?(:name) || !headers.include?(:gender) || !headers.include?(:birth_date)
+        raise "Incorrect Headers. The file should have headers of 'Shop','Name','Gender', 'Birth Date'" 
+      end  
+    end
 
-#     def download_csv_file(payload)
-#       s3 = AWS::S3.new(
-#         :access_key_id => SiteConfig.aws_access_key_id,
-#         :secret_access_key => SiteConfig.aws_secret_access_key)
-#       bucket = s3.buckets["optyn#{Rails.env}"]
-#       content = bucket.objects[CGI::unescape(payload.filepath)].read
-#     end#end of function download_csv_file
+    def download_csv_file(payload)
+      s3 = AWS::S3.new(
+        :access_key_id => SiteConfig.aws_access_key_id,
+        :secret_access_key => SiteConfig.aws_secret_access_key)
+      bucket = s3.buckets["optyn#{Rails.env}"]
+      content = bucket.objects[CGI::unescape(payload.filepath)].read
+    end#end of function download_csv_file
   end # end of the shops importer module
 end #end of the shops module
