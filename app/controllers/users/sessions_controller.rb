77c62a714a8cfd88cd632ceb2128519542dafca9
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-10-23 20:20:59 +0530

    Identify shop by its uuid

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -89,7 +89,7 @@ class Users::SessionsController < Devise::SessionsController
 
     if !saved && @user.errors.blank?
       @user.show_password = true
-      @shop = Shop.by_app_id(params[:app_id])
+      @shop = Shop.by_app_id(params[:app_id]) rescue Shop.for_uuid(params[:uuid])
       @user.show_shop = true
       @user.shop_identifier = @shop.id
       @user.save(validate: false)
