f61d3cab7578cdd8ff1b81a8247fd9f48ad7cd4c
Anagha <anagha@idyllic-software.com>
2013-10-24 14:56:32 +0530

    Added edit form in the popup.

--- app/assets/javascripts/merchants/connections.js
+++ app/assets/javascripts/merchants/connections.js
@@ -4,4 +4,38 @@ $(document).ready(function(){
      //var userId = $(".open-infoDialog").val($(this).data('id'));
      $('#infoDialog').modal('show');
    });
+
+  $(".edit_user").click(function(){
+    var id = $(this).attr('id');
+    $.ajax({
+      url: $('#edit_path_' + id).val(),
+      type: 'POST',
+      success: function (data) {
+        $("#infoDialog_" + id + " .modal-body").html( data );
+      }
+    });
+  });
+
+  $(".update_user").click(function(){
+    var id = $(this).attr('id');
+    var form_url = $("#edit_user_form").attr('action');
+    $.ajax({
+      url: form_url,
+      type: 'POST',
+      data: {name: $("#user_name").val(), email: $("#user_email").val()},
+      success: function (data) {
+        if(data.search("User updated successfully.") != '-1')
+        {
+          alert(data)
+          window.location.replace(window.location.pathname);
+        }
+        else
+          $("#infoDialog_" + id + " .modal-body").html( data );
+      },
+      error: function (jqXHR, textStatus, errorThrown)
+      {
+        alert(errorThrown)
+      }
+    });
+  });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -215,6 +215,7 @@ Optyn::Application.routes.draw do
     resources :connections do
       collection do
         match 'unsubscribe/:id', to: "connections#unsubscribe_user", as: :unsubscribe
+        match 'update_user/:id', to: "connections#update_user", as: :update_user
         match 'add_user', to: "connections#add_user", as: :add_user
         match 'create_user', to: "connections#create_user", as: :create_user
         match 'edit_user/:id', to: "connections#edit", as: :edit_user
