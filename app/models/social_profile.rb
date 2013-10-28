3476ec562ce6a7874474800ba4a63ce2ad04fc57
Anagha <anagha@idyllic-software.com>
2013-10-28 12:16:48 +0530

    Added url validation.

--- app/models/social_profile.rb
+++ app/models/social_profile.rb
@@ -5,5 +5,7 @@ class SocialProfile < ActiveRecord::Base
   SOCIAL_PROFILES = [["1", "facebook"], ["2", "twitter"], ["3", "linkedin"]]
 
   validates :sp_link, uniqueness: true, :presence => true
-  validates :sp_type, :presence => true
+  validates :sp_type, uniqueness: true,	:presence => true
+
+  validates_format_of :sp_link, :with => URI::regexp(%w(http https))
 end
