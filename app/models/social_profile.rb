9bfb8c81eadb0e8307955567f6090ea52e83ef5d
Anagha <anagha@idyllic-software.com>
2013-10-30 13:57:02 +0530

    Added multiple textboxes for social profiles.

--- app/models/social_profile.rb
+++ app/models/social_profile.rb
@@ -2,13 +2,10 @@ class SocialProfile < ActiveRecord::Base
   belongs_to :shop
   attr_accessible :sp_link, :sp_type
 
-  SOCIAL_PROFILES = [["1", "facebook"], ["2", "twitter"], ["3", "linkedin"]]
+  SOCIAL_PROFILES = {"facebook" => 1, "twitter" => 2, "linkedin" => 3}
 
-  validates :sp_link, :presence => true
-  validates :sp_type,	:presence => true
-
-  validates_format_of :sp_link, :with => URI::regexp(%w(http https))
+  validates_format_of :sp_link, :with => URI::regexp(%w(http https)), unless: Proc.new{|s| s.sp_link.blank?}
   
-  validates_uniqueness_of :sp_link, :scope => [:shop_id]
-  validates_uniqueness_of :sp_type, :scope => [:shop_id]
+  validates_uniqueness_of :sp_link, :scope => [:shop_id], unless: Proc.new{|s| s.sp_link.blank?}
+  validates_uniqueness_of :sp_type, :scope => [:shop_id], unless: Proc.new{|s| s.sp_link.blank?}
 end
