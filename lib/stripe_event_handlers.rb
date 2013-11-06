24eb67b8eb9523a40fb676990bf27f89d3a4de1d
Shashank Singh <shashank.personal@gmail.com>
2013-11-06 12:49:26 +0530

    fixing charge successded

--- lib/stripe_event_handlers.rb
+++ lib/stripe_event_handlers.rb
@@ -133,19 +133,19 @@ module StripeEventHandlers
   def self.handle_charge_succeeded(params)
     binding.pry
     Charge.create(
-        :created => params[:created] rescue nil ,
-        :live_mode => params[:livemode] rescue nil ,
-        :fee_amount => params[:data][:object][:fee] rescue nil ,
-        :invoice => params[:data][:object][:invoice]rescue nil ,
-        :description => params[:data][:object][:description] rescue nil ,
-        :dispute => params[:data][:object][:dispute] rescue nil ,
-        :refunded => params[:data][:object][:refunded]rescue nil ,
-        :paid => params[:data][:object][:paid] rescue nil ,
-        :amount => params[:data][:object][:amount] rescue nil ,
-        :card_last4 => params[:data][:object][:card][:last4] rescue nil ,
-        :amount_refunded => params[:data][:object][:amount_refunded] rescue nil ,
-        :customer => params[:data][:object][:customer] rescue nil ,
-        :fee_description => params[:data][:object][0][:fee_details] rescue nil
+        :created => params[:created]  ,
+        :live_mode => params[:livemode]  ,
+        :fee_amount => params[:data][:object][:fee] ,
+        :invoice => params[:data][:object][:invoice] ,
+        :description => params[:data][:object][:description] ,
+        :dispute => params[:data][:object][:dispute]  ,
+        :refunded => params[:data][:object][:refunded] ,
+        :paid => params[:data][:object][:paid] ,
+        :amount => params[:data][:object][:amount]  ,
+        :card_last4 => params[:data][:object][:card][:last4] ,
+        :amount_refunded => params[:data][:object][:amount_refunded]  ,
+        :customer => params[:data][:object][:customer]  ,
+        :fee_description => params[:data][:object][0][:fee_details]
       )
   end
 
