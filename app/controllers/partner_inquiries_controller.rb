3dfdd763672efa8a49e390d5e0ae304593048e89
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-11-06 16:07:35 +0530

    [58367278] Ajaxify the partner form

--- app/controllers/partner_inquiries_controller.rb
+++ app/controllers/partner_inquiries_controller.rb
@@ -6,11 +6,9 @@ class PartnerInquiriesController < ApplicationController
   def create
     @partner_inquiry = PartnerInquiry.new(params[:partner_inquiry])
     @partner_inquiry.save!
-    flash[:notice] = "Inquiry submitted successfully. We will be in touch within 24 hours or less"
     Resque.enqueue(PartnerInquirySender, @partner_inquiry.id)
-    redirect_to  new_partner_inquiry_path
+    head :ok
   rescue ActiveRecord::RecordInvalid => e
-    flash[:error] = "Could not submit the inquiry form"
-    render action: :new
+    render partial: 'form', status: :unprocessable_entity
   end
 end
