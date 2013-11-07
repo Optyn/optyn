8dc2014e77c66b6c26a1e5c79c90ad18c1e6d11a
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-11-07 14:04:53 +0530

    renaming columns

--- db/migrate/20131101141721_create_charges.rb
+++ db/migrate/20131101141721_create_charges.rb
@@ -1,19 +1,20 @@
 class CreateCharges < ActiveRecord::Migration
   def change
     create_table :charges do |t|
+      t.string :stripe_charge_id, index: true
       t.integer :created
-      t.string :live_mode
+      t.boolean :livemode
       t.integer :fee_amount
-      t.string :invoice
-      t.string :description
+      t.string :stripe_invoice_id
+      t.text :description
       t.string :dispute
-      t.string :refunded
-      t.string :paid
+      t.boolean :refunded
+      t.boolean :paid
       t.integer :amount
       t.integer :card_last4
       t.integer :amount_refunded
-      t.string :customer
-      t.string :fee_description
+      t.string :stripe_customer_token
+      t.text :fee_description
       t.references :invoice
 
       t.timestamps
