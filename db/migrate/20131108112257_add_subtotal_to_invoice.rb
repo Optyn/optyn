1178c4664024a42f3d3479f742bb3fe1fafdce4f
Shashank Singh <shashank.personal@gmail.com>
2013-11-08 16:56:25 +0530

    added stripe coupon token

---
+++ db/migrate/20131108112257_add_subtotal_to_invoice.rb
@@ -0,0 +1,7 @@
+class AddSubtotalToInvoice < ActiveRecord::Migration
+  def change
+    add_column :invoices, :subtotal, :integer
+    add_column :invoices, :total, :integer
+    add_column :invoices, :stripe_coupon_token, :string
+  end
+end
