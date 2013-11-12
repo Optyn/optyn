61719c2e6035ea644bbc540039d677b0bee6cd1f
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-11-12 11:39:13 +0530

    Knock off the sendmail settings

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,3 @@ Optyn::Application.initialize!
 
 HUMAN_DATE_FORMAT = '%m/%d/%Y'
 HUMAN_TIME_FORMAT = '%m/%d/%Y %H:%M:%S'
-ActionMailer::Base.delivery_method = :sendmail
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
