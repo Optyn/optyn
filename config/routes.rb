db043d6c0153043478e59f2c1d37492030c1fb6e
Alen Malkoc <alen@zimamail.com>
2013-11-17 15:20:03 -0600

    resources > loyalty marketing

--- config/routes.rb
+++ config/routes.rb
@@ -67,6 +67,7 @@ Optyn::Application.routes.draw do
   match '/resources/contests' => 'main#resources_contests'
   match '/resources/coupons' => 'main#resources_coupons'
   match '/resources/customer-retention' => 'main#resources_customer_retention'
+  match '/resources/loyalty-marketing' => 'main#resources_loyalty_marketing'
 
   #named routes partner inquiry
   get "/partner-with-us", to: 'partner_inquiries#new', as: :new_partner_inquiry
