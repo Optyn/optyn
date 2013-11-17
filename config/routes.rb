1cfe196ba4bf6d5576ec34f5ef0da53d527fe5ed
Alen Malkoc <alen@zimamail.com>
2013-11-17 15:08:49 -0600

    resources > coupons

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ Optyn::Application.routes.draw do
   match '/resources/email-marketing/mobile-responsive-emails' => 'main#resources_mobile_responsive_emails'
   match '/resources/digital-marketing' => 'main#resources_digital_marketing'
   match '/resources/contests' => 'main#resources_contests'
-  
+  match '/resources/coupons' => 'main#resources_coupons'
 
   #named routes partner inquiry
   get "/partner-with-us", to: 'partner_inquiries#new', as: :new_partner_inquiry
