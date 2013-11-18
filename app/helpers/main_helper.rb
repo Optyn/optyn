24e4dc5a370e9acb9a49a634fad42ba56b0bfa6b
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-11-18 20:07:42 +0530

    [60931186] changing the button caption and link based on merchants login on the pricing page

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,10 @@
 module MainHelper
+  def pricing_navigation_link
+    # %a.btn.btn-primary.btn-large{ href: new_merchants_manager_registration_path } Get Started
+    if merchants_manager_signed_in?
+      link_to("Back to Pricing", merchants_upgrade_subscription_path, class: "btn btn-primary btn-large")
+    else
+      link_to("Get Started", new_merchants_manager_registration_path, class: "btn btn-primary btn-large")
+    end
+  end
 end
