887151f33871eb744d42ac8679de79df862fb093
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-11-19 13:25:20 +0530

    [60389776] Show the missing error on discount amount

--- app/models/coupon_message.rb
+++ app/models/coupon_message.rb
@@ -19,13 +19,13 @@ class CouponMessage < Message
   
   private
   def validate_discount_amount
-    return self.errors.add(:base, "You need to add the discount amount") if discount_amount.blank?
+    return self.errors.add(:discount_amount, "Please add the discount amount") if discount_amount.blank?
     numeric_amount = discount_amount.to_i
 
     if percentage_off?
-      self.errors.add(:base, "Please add valid values between 0 - 100") if numeric_amount <= 0 || numeric_amount > 100
+      self.errors.add(:discount_amount, "Please add valid values between 0 - 100") if numeric_amount <= 0 || numeric_amount > 100
     else
-      self.errors.add(:base, "Please make sure you add a numeric value") if numeric_amount <= 0
+      self.errors.add(:discount_amount, "Please make sure you add a numeric value") if numeric_amount <= 0
     end
   end
 end
