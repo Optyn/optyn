bd0f4e3195f335cebc063c754a3bccb9c3ed6692
Shashank Singh <shashank.personal@gmail.com>
2013-11-19 21:21:46 +0530

    for debug : rails logger in view, and resuce nil added to active_Card

--- app/controllers/merchants/subscriptions_controller.rb
+++ app/controllers/merchants/subscriptions_controller.rb
@@ -109,7 +109,7 @@ class Merchants::SubscriptionsController < Merchants::BaseController
           @subscription.update_attribute(:active, true)
           amount = @customer.subscription.plan.amount
           conn_count = current_shop.active_connection_count
-          last4 = @customer.active_card.last4
+          last4 = @customer.active_card.last4 rescue nil
           Resque.enqueue(PaymentNotificationSender, 'MerchantMailer', 'payment_notification', {shop_id: current_shop, amount: amount, conn_count: conn_count, last4: last4})
           flash[:notice]="Payment done successfully"
           redirect_to (session[:return_to] || root_path)
