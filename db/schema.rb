b165c107a25337ecdfdd1053d460045d72997192
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-11-19 17:36:04 +0530

    Show the footer correctly in the preview and sent view.  Refactor the footer to make it more readable

--- db/schema.rb
+++ db/schema.rb
@@ -516,12 +516,12 @@ ActiveRecord::Schema.define(:version => 20131115093707) do
     t.integer  "email_box_click_count",      :default => 0
     t.integer  "coupon_id"
     t.datetime "discount_end_at"
-    t.integer  "partner_id"
-    t.string   "uuid"
-    t.string   "header_background_color",    :default => "#1791C0"
     t.string   "phone_number",               :default => ""
+    t.string   "header_background_color",    :default => "#1791C0"
     t.datetime "deleted_at"
     t.boolean  "pre_added",                  :default => false
+    t.integer  "partner_id"
+    t.string   "uuid"
     t.string   "footer_background_color",    :default => "#ffffff"
     t.boolean  "affiliate_tracker_pinged",   :default => false
   end
@@ -564,7 +564,6 @@ ActiveRecord::Schema.define(:version => 20131115093707) do
     t.integer  "user_id"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
-    t.integer  "label_id"
   end
 
   add_index "survey_answers", ["created_at"], :name => "index_survey_answers_on_created_at"
