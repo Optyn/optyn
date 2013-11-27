242d62b3122f88abfe694b3b4170d218795fd7ce
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-11-27 15:54:38 +0530

    Show the shop logo

--- app/controllers/api/v1/merchants/messages_controller.rb
+++ app/controllers/api/v1/merchants/messages_controller.rb
@@ -63,6 +63,7 @@ module Api
         def show
           @message = Message.for_uuid(params[:id])
           @shop = @message.shop
+          @shop_logo = true
           @partner = @shop.partner
           populate_labels
           @rendered_string = render_to_string(:template => 'api/v1/merchants/messages/preview_email', :layout => false, :formats=>[:html],:handlers=>[:haml])
