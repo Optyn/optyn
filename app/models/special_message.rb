05d932fd3084f0206f71cb62f18594a5075701cb
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-12-02 17:33:36 +0530

    Adding discount fields to special message type

--- app/models/special_message.rb
+++ app/models/special_message.rb
@@ -1,6 +1,8 @@
 class SpecialMessage < Message
-  attr_accessible :content, :ending, :fine_print
+  attr_accessible :content, :type_of_discount, :discount_amount, :ending, :fine_print, :coupon_code
 
   validates :content, presence: true
+  validates :type_of_discount, presence: true
+  validate :validate_discount_amount
   validate :validate_ending
 end
