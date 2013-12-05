179f2c563c43b283c446325a35622634d3fe5bbd
Shashank Singh <shashank.personal@gmail.com>
2013-12-05 15:48:21 +0530

    helper now has defination charge--->plan

--- app/helpers/merchants/subscription_helper.rb
+++ app/helpers/merchants/subscription_helper.rb
@@ -1,7 +1,16 @@
 module Merchants::SubscriptionHelper
 	#takes stripe_charge_id and returns plan object
 	def get_plan_from_charge(charge)
-		binding.pry
+		stripe_invoice_id = charge.stripe_invoice_token
+		invoice = Invoice.where(:stripe_invoice_id=>stripe_invoice_id) rescue nil
+		stripe_plan_token = invoice.stripe_plan_token rescue nil
+		if !stripe_plan_token.nil?
+			plan = Plan.where(:plan_id=>stripe_plan_token)
+			return plan
+		else
+			return nil
+		end
 	end
+
 
 end
