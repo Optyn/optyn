35bae94a037d77be964bcca1fe229ffaf4fda142
Shashank Singh <shashank.personal@gmail.com>
2013-12-17 12:35:16 +0530

    little more fixes to god file

--- god/resque.rb
+++ god/resque.rb
@@ -1,39 +1,48 @@
-APPLICATION_ROOT = '/home/dev/code/helping-faceless'
-RAILS_ENV = 'production'
-DIR = File.join(APPLICATION_ROOT,'current')
-SIDKIQ_PID = File.join(APPLICATION_ROOT,'shared/pids/sidekiq.pid')
-PID_FILE = "/var/run/"
-GOD_LOG = File.join(APPLICATION_ROOT,'shared/log/sidekiq.log')
-God.pid_file_directory = PID_FILE
 God.watch do |w|
-  w.uid = 'dev'
-  w.gid = 'dev'
-  w.name = 'sidekiq'
-  w.dir = DIR
+  w.name = 'resque'
   w.interval = 30.seconds
-  w.start = "/bin/bash -c 'cd #{DIR} &&  /usr/bin/env bundle exec sidekiq -e #{RAILS_ENV} --logfile #{GOD_LOG} --pidfile #{SIDKIQ_PID} -d'"
-  w.log = "#{APPLICATION_ROOT}/shared/log/god_sidekiq.log"
-  w.start_grace = 30.seconds
-  w.stop_signal = 'KILL'
-  w.pid_file = SIDKIQ_PID
-  w.behavior(:clean_pid_file)
-
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 5.seconds
-      c.running = false
+  w.env = { 'RAILS_ENV' => 'production',
+  'QUEUE' => '*' }
+  w.uid = 'user'
+  w.gid = 'user'
+  w.dir = File.expand_path(File.join(File.dirname(__FILE__),'..'))
+  w.start = "/usr/local/bin/rake resque:work"
+  w.start_grace = 10.seconds
+  w.log = File.expand_path(File.join(File.dirname(__FILE__), '..','log','resque-worker.log'))
+  # restart if memory gets too high
+  w.transition(:up, :restart) do |on|
+    on.condition(:memory_usage) do |c|
+    c.above = 200.megabytes
+    c.times = 2
     end
   end
-
-  w.restart_if do |restart|
-    restart.condition(:memory_usage) do |c|
-      c.above = 300.megabytes
-      c.times = [3, 5] # 3 out of 5 intervals
+   
+  # determine the state on startup
+  w.transition(:init, { true => :up, false => :start }) do |on|
+    on.condition(:process_running) do |c|
+    c.running = true
     end
-
-    restart.condition(:cpu_usage) do |c|
-      c.above = 50.percent
-      c.times = 5
+  end
+   
+  # determine when process has finished starting
+  w.transition([:start, :restart], :up) do |on|
+    on.condition(:process_running) do |c|
+    c.running = true
+    c.interval = 5.seconds
+    end
+   
+    # failsafe
+    on.condition(:tries) do |c|
+    c.times = 5
+    c.transition = :start
+    c.interval = 5.seconds
+    end
+  end
+     
+  # start if process is not running
+  w.transition(:up, :start) do |on|
+    on.condition(:process_running) do |c|
+    c.running = false
     end
   end
 end
