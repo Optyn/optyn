07a51f94efa4e61fff4ba3f3238f5eac19bfd42a
Shashank Singh <shashank.personal@gmail.com>
2013-12-17 12:51:46 +0530

    addded a reqsue startup and scheduler script taking following document inr reference http://blog.flowdock.com/2012/08/29/capistrano-god-bundler-resque-configuration/

--- god/resque.rb
+++ god/resque.rb
@@ -1,49 +1,58 @@
-God.watch do |w|
-  w.name = 'resque'
-  w.interval = 30.seconds
-  w.env = { 'RAILS_ENV' => 'production',
-  'QUEUE' => '*' }
-  w.uid = 'user'
-  w.gid = 'user'
-  w.dir = File.expand_path(File.join(File.dirname(__FILE__),'..'))
-  w.start = "/usr/local/bin/rake resque:work"
-  w.start_grace = 10.seconds
-  w.log = File.expand_path(File.join(File.dirname(__FILE__), '..','log','resque-worker.log'))
-  
-  # restart if memory gets too high
-  w.transition(:up, :restart) do |on|
-    on.condition(:memory_usage) do |c|
-    c.above = 200.megabytes
-    c.times = 2
+rails_env   = ENV['RAILS_ENV']
+raise "Please specify RAILS_ENV." unless rails_env
+rails_root  = ENV['RAILS_ROOT'] || File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+
+# RESQUE_PROCESSORS = [
+#   [1, "foo,bar"],
+#   [2, "baz"],
+#   [3, "demo"]
+# ]
+
+RESQUE_PROCESSORS.each do |resque_id, resque_queues|
+  God.watch do |w|
+    w.dir      = "#{rails_root}"
+    w.name     = "resque-#{resque_id}"
+    w.group    = 'resque'
+    w.interval = 30.seconds
+    w.env      = {"QUEUES"=>resque_queues, "RAILS_ENV"=>rails_env, "BUNDLE_GEMFILE"=>"#{rails_root}/Gemfile"}
+    w.start    = "bundle exec rake -f #{rails_root}/Rakefile environment resque:work"
+    w.log      = "#{rails_root}/log/resque-#{resque_id}.log"
+
+    # restart if memory gets too high
+    w.transition(:up, :restart) do |on|
+      on.condition(:memory_usage) do |c|
+        c.above = 350.megabytes
+        c.times = 2
+      end
     end
-  end
-   
-  # determine the state on startup
-  w.transition(:init, { true => :up, false => :start }) do |on|
-    on.condition(:process_running) do |c|
-    c.running = true
+
+    # determine the state on startup
+    w.transition(:init, { true => :up, false => :start }) do |on|
+      on.condition(:process_running) do |c|
+        c.running = true
+      end
     end
-  end
-   
-  # determine when process has finished starting
-  w.transition([:start, :restart], :up) do |on|
-    on.condition(:process_running) do |c|
-    c.running = true
-    c.interval = 5.seconds
+
+    # determine when process has finished starting
+    w.transition([:start, :restart], :up) do |on|
+      on.condition(:process_running) do |c|
+        c.running = true
+        c.interval = 5.seconds
+      end
+
+      # failsafe
+      on.condition(:tries) do |c|
+        c.times = 5
+        c.transition = :start
+        c.interval = 5.seconds
+      end
     end
-   
-    # failsafe
-    on.condition(:tries) do |c|
-    c.times = 5
-    c.transition = :start
-    c.interval = 5.seconds
-    end
-  end
-     
-  # start if process is not running
-  w.transition(:up, :start) do |on|
-    on.condition(:process_running) do |c|
-    c.running = false
+
+    # start if process is not running
+    w.transition(:up, :start) do |on|
+      on.condition(:process_running) do |c|
+        c.running = false
+      end
     end
   end
 end
