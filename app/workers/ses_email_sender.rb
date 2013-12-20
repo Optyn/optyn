93415a59e3af3e91292b48ee8bb8f8e1f4f8af79
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2013-12-20 16:51:47 +0530

    Convert logging statements to puts

--- app/workers/ses_email_sender.rb
+++ app/workers/ses_email_sender.rb
@@ -2,13 +2,37 @@ class SesEmailSender
   @queue = :message_queue
 
   def self.perform(message_email_auditor_id)
+    start_time = Time.now.to_i
+    puts "~"*100
+    puts  "#{message_email_auditor_id} --> #{Time.now().to_i}"
     message_email_auditor = MessageEmailAuditor.find(message_email_auditor_id)
+    puts  "#{message_email_auditor_id} --> #{Time.now().to_i}"
     message_user = message_email_auditor.message_user
     message = message_user.message
 
-    ses_message = MessageMailer.send_announcement(message, message_user).deliver
+    puts "+"*100
+    ses_message_time_start = Time.now()
+    puts  "#{message_email_auditor_id} --> #{ses_message_time_start}"
+    
+
+    begin
+      ses_message = MessageMailer.send_announcement(message, message_user).deliver
+    rescue Exception => ex
+      puts ex
+    end
+    
+    puts  "#{message_email_auditor_id} --> #{Time.now()}"
+    ses_message_time_end = Time.now().to_i
+
     message_email_auditor.update_attributes(delivered: true, ses_message_id: ses_message['message_id'].to_s.split(/@/).first)
+    puts  "#{message_email_auditor_id} --> #{Time.now().to_i}"
     message = message_email_auditor.message_user.message
-    message.destroy if message.instance_of?(VirtualMessage) 
+    message.destroy if message.instance_of?(VirtualMessage)
+    puts "#{message_email_auditor_id} --> #{Time.now().to_i}"
+    puts "#{message_email_auditor_id} Time for SES API hit"
+    # puts ses_message_time_end - ses_message_time_start
+    puts "~"*100
+    end_time = Time.now.to_i
+    puts "Total Time: #{end_time - start_time}"
   end
 end
