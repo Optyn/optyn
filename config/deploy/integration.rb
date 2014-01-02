d1d2047a53abde51b84d21343082aaa2aaa13df6
gauravgaglani <gaurav.gaglani@gmail.com>
2014-01-02 20:47:20 +0530

    Remove the resque worker entry

--- config/deploy/integration.rb
+++ config/deploy/integration.rb
@@ -2,4 +2,3 @@ server "ec2-23-22-29-198.compute-1.amazonaws.com", :web, :app, :db, :messenger, 
 set :branch, "development"
 set :rails_env, 'staging'
 set :local_app_url, 'http://localhost:3000/'
-set :workers, {"*" => 1}
