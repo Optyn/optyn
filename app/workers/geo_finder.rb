ba8b564a1ed462e3e32a463b4df243a24f07c39c
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-01-04 14:26:19 +0530

    Adding geo_finder tasks to general_queue

--- app/workers/geo_finder.rb
+++ app/workers/geo_finder.rb
@@ -1,7 +1,8 @@
 class GeoFinder
   include Sidekiq::Worker
+  sidekiq_options :queue => :general_queue
+	# @queue = :general_queue
   
-	@queue = :general_queue
 	def perform(location_id)
 		TrackLatLng.fetch_lat_lng(location_id)
 	end
