363f28116875716fb2749ec083846a44cabb8668
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-01-04 14:30:45 +0530

    Adding shared forwerder on the message queue

--- app/workers/shared_forwarder.rb
+++ app/workers/shared_forwarder.rb
@@ -1,6 +1,7 @@
 class SharedForwarder
   include Sidekiq::Worker
-  @queue = :message_queue
+  sidekiq_options :queue => :message_queue
+  # @queue = :message_queue
 
   def perform(user_email, message_id, message_email_auditor_id)
   	message = Message.find(message_id)
