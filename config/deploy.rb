1ead3f43ad7e803498ad2483c696e5f448fc72a8
Akshay <chetan@idyllic-software.com>
2014-01-09 18:02:43 +0530

    change release_path path to current_path for seed task.

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,7 +82,7 @@ namespace :deploy do
 
   desc "reload the database with seed data"
   task :seed do
-    run "cd #{release_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
   end
 
   desc "Make sure local git is in sync with remote."
