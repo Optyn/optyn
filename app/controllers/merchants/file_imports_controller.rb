f17d831160e6a7ace613693d464937ab8b7a9ba7
Akshay <chetan@idyllic-software.com>
2014-01-15 20:04:19 +0530

    [63682234] Added ability to add multiple labels for import customers

--- app/controllers/merchants/file_imports_controller.rb
+++ app/controllers/merchants/file_imports_controller.rb
@@ -1,15 +1,16 @@
 class Merchants::FileImportsController < Merchants::BaseController
 
-
   def index
+    @labels = current_shop.labels
     @file_imports = current_manager.file_imports.order("created_at DESC")
     @file_import = FileImport.new
   end
 
   def create
-    @file_import = FileImport.new(params[:file_import])
-    @file_import.manager_id = current_manager.id
 
+    @file_import = FileImport.new(params[:file_import])
+    @file_import.label = params[:label_ids].join(",")
+    @file_import.manager_id = current_manager.id
     if @file_import.save
       UserImporter.perform_async(@file_import.id)
       redirect_to merchants_file_imports_path, notice: "Your CSV file has been queued for import. We will send you an email with the statistics once the import is complete."
