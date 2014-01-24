b5f1721c77334c85f61789883a8ca897fa4e8559
Akshay Khole <akshay@idyllic-software.com>
2014-01-24 11:43:48 +0530

    handling validation cases
    rendering upload page itself on error

--- app/controllers/template_uploads_controller.rb
+++ app/controllers/template_uploads_controller.rb
@@ -8,10 +8,13 @@ class TemplateUploadsController < ApplicationController
   def create
     @template_upload = TemplateUpload.new
     @template_upload.manager_id = current_manager.id
-    @template_upload.template_html_file = params[:template_upload][:template_html_file].read
+    @template_upload.template_html_file = params[:template_upload][:template_html_file].read rescue nil
     if @template_upload.save
       @template_upload.save_content
+      redirect_to "/merchants/messages/#{params[:message_id]}/template"
+    else
+      @message = Message.select(:uuid).find_by_uuid(params[:message_id])
+      render :new
     end
-    redirect_to "/merchants/messages/#{params[:message_id]}/template"
   end
 end
