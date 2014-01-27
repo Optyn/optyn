04c098da5f6dd268f2e8d6e23537cec61b846557
Akshay Khole <akshay@idyllic-software.com>
2014-01-27 11:46:41 +0530

    Changing namespace for template uploads controller to merchanges namespace

--- app/controllers/merchants/template_uploads_controller.rb
+++ app/controllers/merchants/template_uploads_controller.rb
@@ -1,4 +1,4 @@
-class TemplateUploadsController < ApplicationController
+class Merchants::TemplateUploadsController < Merchants::BaseController
 
   def new
     @template_upload = @template_upload = TemplateUpload.new
@@ -8,7 +8,7 @@ class TemplateUploadsController < ApplicationController
   def create
     @template_upload = TemplateUpload.new
     @template_upload.manager_id = current_manager.id
-    @template_upload.template_html_file = params[:template_upload][:template_html_file].read rescue nil
+    @template_upload.template_html_file = params[:merchants_template_upload][:template_html_file].read rescue nil
     if @template_upload.save
       @template_upload.save_content
       redirect_to "/merchants/messages/#{params[:message_id]}/template"
