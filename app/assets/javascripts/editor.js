eb15de74f42288c724d26f6521b508513cd2011a
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-01-29 12:13:48 +0530

    Fix the relogin issue

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -91,6 +91,7 @@ OP = (function($, window, doucument, Optyn){
           htmlVal += '<div class="blank-space"></div><div class="row-fluid" id="' + row_id + '">' +
             '<div class="span4">Preview:<br /><img src="' + divisionContents.imageURLs[count] + '" /></div>' +
             '<div><form class="msg_img_upload" action="' + image_form_action + '" method="post" enctype="multipart/form-data" data-remote="true" >' +
+            '<input type="hidden" name="authenticity_token" value="' + $('#authenticity_token').val() + '" />' +
             '<input type="hidden" name="imagerow" value="' + row_id +'" />' +
             '<div class="span8">Upload:<br /><input type="file" name="imgfile" accept=".jpg,.png,.gif,.jpeg"><br />' +
             '<input type="submit" value="Upload image" class="upload-img-btn btn btn-success btn-small" /></div></div>' +
