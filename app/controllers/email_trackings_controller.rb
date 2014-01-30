828eeac2fbc5abfa1110fa4cf27d7425dbd472dc
Akshay Khole <akshay@idyllic-software.com>
2014-01-30 11:09:13 +0530

    refactoring more code

--- app/controllers/email_trackings_controller.rb
+++ app/controllers/email_trackings_controller.rb
@@ -1,8 +1,9 @@
 class EmailTrackingsController < ApplicationController
   def index
+    return nil if !params[:token].present?
     data = Encryptor.decrypt_for_template(params[:token])
     EmailTracking.new.track(data, current_manager.id)
-    redirect_url = TemplateUrl.find(data[:template_url_id]).original_url
+    redirect_url = TemplateUrl.select(:original_url).find(data[:template_url_id]).original_url
     redirect_to redirect_url
   end
 end
