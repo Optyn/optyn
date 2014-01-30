be9efcb5ff52979209de8c554abee22b8ef9b99f
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-01-30 19:22:14 +0530

    Fixing the highlighting of the message menu item

--- app/helpers/merchants/messages_helper.rb
+++ app/helpers/merchants/messages_helper.rb
@@ -132,7 +132,7 @@ module Merchants::MessagesHelper
       return highlight_class
     end
 
-    return highlight_class if !["new", "edit", "create", "update", "types", "preview", "create_response_message", "launch"].include?(action_name) && registered_action == link_name.to_s.gsub("&nbsp;", "").strip.downcase
+    return highlight_class if !["new", "edit", "create", "update", "types", "preview", "create_response_message", "launch", "new_template", "template", "edit_template", "preview_template"].include?(action_name) && registered_action == link_name.to_s.gsub("&nbsp;", "").strip.downcase
   end
 
   def message_detail_date(message)
