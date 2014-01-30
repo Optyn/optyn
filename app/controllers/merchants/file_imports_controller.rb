603dade93b548dbb7c9e222f66cdcdbb8d32a6b1
Akshay <chetan@idyllic-software.com>
2014-01-30 14:10:50 +0530

    [64731758] System broke - on .png file upload - Fixed

--- app/controllers/merchants/file_imports_controller.rb
+++ app/controllers/merchants/file_imports_controller.rb
@@ -11,6 +11,7 @@ class Merchants::FileImportsController < Merchants::BaseController
     @file_import = FileImport.new(params[:file_import])
     @file_import.label = params[:label_ids].join(",") rescue ""
     @file_import.manager_id = current_manager.id
+    @labels = current_shop.labels
     if @file_import.save
       UserImporter.perform_async(@file_import.id)
       redirect_to merchants_file_imports_path, notice: "Your CSV file has been queued for import. We will send you an email with the statistics once the import is complete."
