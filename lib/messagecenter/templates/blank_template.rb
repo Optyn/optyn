f5762436661bf7eeff45898ee9db192b3c7179fe
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-02-11 13:37:00 +0530

    Bug fix: when a ' is used in the shop name

--- lib/messagecenter/templates/blank_template.rb
+++ lib/messagecenter/templates/blank_template.rb
@@ -74,7 +74,7 @@ module Messagecenter
           html = []
           row.grids.each do |grid|
             components_json = add_toolset_to_components(grid.data_model) 
-            data_model = %{<span style="width:0px;height:0px;" class="data-components" data-component-type="#{grid.data_model['type']}" data-components='#{components_json}'></span>}  
+            data_model = %{<span style="width:0px;height:0px;" class="data-components" data-component-type="#{grid.data_model['type']}" data-components='#{components_json.gsub("'", "\\'")}''></span>}  
             content =  raw(grid.html.gsub(Template::PLACE_HOLDER_ELEM, (data_model + static_toolset_markup(grid.data_model) + grid.divisions.first.html)))
             html << content
           end
