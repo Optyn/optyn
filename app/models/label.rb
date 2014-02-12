79ac6e0d68ce46a2fcbcc5291aaf20127b7099a0
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-02-12 13:32:59 +0530

    Removing dependent destroy and deleting the dependencis through callback in labels due to performance reasons

--- app/models/label.rb
+++ app/models/label.rb
@@ -2,8 +2,10 @@ class Label < ActiveRecord::Base
   belongs_to :shop
   belongs_to :survey_answer
   
-  has_many :user_labels, dependent: :destroy
-  has_many :message_labels, dependent: :destroy
+  has_many :user_labels #, dependent: :delete
+  has_many :message_labels #, dependent: :delete
+
+  before_destroy :bulk_delete_user_lables, :bulk_delete_message_labels
 
   attr_accessible :shop_id, :name, :survey_answer_id
 
@@ -34,4 +36,13 @@ class Label < ActiveRecord::Base
   def select_all?
     inactive? && SELECT_ALL_NAME == name
   end
+
+  private
+    def bulk_delete_user_lables
+      UserLabel.where(label_id: self.id).delete_all
+    end
+
+    def bulk_delete_message_labels
+      MessageLabel.where(id: self.id).delete_all
+    end
 end
