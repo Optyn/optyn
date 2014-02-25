ba3529f77e4c5db7b3f28c93297b4da860f25a7a
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2014-02-25 10:31:12 +0530

    Fix for email body background colour for non newletter campaign emails.

--- lib/shop_logo.rb
+++ lib/shop_logo.rb
@@ -4,12 +4,12 @@ module ShopLogo
       if shop.website.present?
         content = <<-HTML
           <a here="#{shop.logo_location}" href="#{shop.website}" target="_blank">
-            <img src="#{shop.logo_location}" title="#{shop.name.gsub(/['"]/, "")}" style="margin:5px;" />
+            <img src="#{shop.logo_location}" title="#{shop.name.gsub(/['"]/, "")}" style="margin:5px;max-width:550px;" />
           </a>
         HTML
         content.to_s.html_safe
       else
-        %{<img src="#{shop.logo_location}" title="#{shop.name.gsub(/['"]/, "")}", style="margin:5px;max-height:150px; max-width:100%;" class="nl-width-580" />}.html_safe
+        %{<img src="#{shop.logo_location}" title="#{shop.name.gsub(/['"]/, "")}", style="margin:5px;max-height:150px;max-width:580px;" class="nl-width-580" />}.html_safe
       end
     else
       if shop.website.present?
