8030d7aed3e6713a5f3f3a7ba993bf49d04694f0
Manjiri <patmanjiri@gmail.com>
2014-02-28 14:48:14 +0530

    Fix for [66225338] Edited delete popup message. Removed before_destroy callback

--- app/models/label.rb
+++ app/models/label.rb
@@ -5,8 +5,6 @@ class Label < ActiveRecord::Base
   has_many :user_labels #, dependent: :delete
   has_many :message_labels #, dependent: :delete
 
-  before_destroy :bulk_delete_user_lables, :bulk_delete_message_labels
-
   attr_accessible :shop_id, :name, :survey_answer_id
 
   SELECT_ALL_NAME = 'Select All'
