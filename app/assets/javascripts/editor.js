e296fd4fc6a821adc99cc9fd220535420300a862
Akshay <chetan@idyllic-software.com>
2014-03-04 19:12:53 +0530

    Added validation for link

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -152,7 +152,7 @@ OP = (function($, window, doucument, Optyn){
           '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
           '<h3>Add Link</h3></div>' +
           '<div class="modal-body">' +
-          '<p><input type="url" name="link"></p></div>' +
+          '<p><input type="url" name="link" value="'+ currentArtifact.content[1] +'" id="imageLinkModel'+ row_id+'"></p></div>' +
           '<div class="modal-footer">' +
           '<input type="submit" value="Save changes" class="btn btn-primary saveLinkUrl" data-dismiss="modal" data-image-link-url="AddLink'+ row_id+'" id="populateLink'+ row_id+'"/></div></div>';
 
