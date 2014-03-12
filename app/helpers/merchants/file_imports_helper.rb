b0b9bf4655b964b39ecb007622070e29e82b0342
Akshay <chetan@idyllic-software.com>
2014-03-12 18:30:45 +0530

    Fixed typo

--- app/helpers/merchants/file_imports_helper.rb
+++ app/helpers/merchants/file_imports_helper.rb
@@ -1,6 +1,6 @@
 module Merchants::FileImportsHelper
   STATS = {:user_creation => "New Subscribers", :existing_user => "Existing Subscribers", :unparsed_rows => "Unparsed rows" }
-  def stats_chage
+  def stats_change
     {:user_creation => "New Subscribers", :existing_connection => "Existing Subscribers", :unparsed_rows => "Unparsed rows" }
   end
 
