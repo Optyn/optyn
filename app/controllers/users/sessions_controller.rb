eede80a661edfd78668a0b4c0e9a2d82c67f832e
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-05-19 15:11:37 +0530

    Changes for connection not created

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -65,12 +65,10 @@ class Users::SessionsController < Devise::SessionsController
 
 
   def authenticate_with_email
-
     if params[:user][:email].match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/)
       @user = User.find_by_email(params[:user][:email])
       @user = sudo_registration(params) unless @user.present?
-
-      sign_in @user
+      # sign_in @user
       session[:user_return_to] = nil
 
       respond_to do |format|
