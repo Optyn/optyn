3c2f3453135f99c869cdead81b6662318c7f2945
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-05-19 16:35:49 +0530

    Adding a validation message for invalid email. It will return back to the form.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -81,6 +81,7 @@ class Users::SessionsController < Devise::SessionsController
         @user = User.new
         @user.errors.add(:base, "Please check your email address")
         format.json { render(status: :ok) } 
+        format.html { render(action: 'invalid_email') }
       end
     end
   end
