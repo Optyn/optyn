aba7638cda39d366c0fd5622ff7725e8a2c23d94
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-05-21 16:34:29 +0530

    Credentials for email sending through sendgrid

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -20,14 +20,14 @@ ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
    :secret_access_key => 'jY2yhFWFzr+BAQrjqLEdqHJ3kLRiB1TKlGvXzklK'
 
 ## Sendgrid Settings
-# require File.expand_path('../site_config', __FILE__)
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-#   :address => "smtp.sendgrid.net",
-#   :port => 587,
-#   :domain => "optyn.com",
-#   :authentication => :plain,
-#   :user_name => SiteConfig.send_grid_username,
-#   :password => SiteConfig.send_grid_password,
-#   :enable_starttls_auto => true
-# }
+require File.expand_path('../site_config', __FILE__)
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :domain => "simple-send.com",
+  :authentication => :plain,
+  :user_name => SiteConfig.send_grid_username,
+  :password => SiteConfig.send_grid_password,
+  :enable_starttls_auto => true
+}
