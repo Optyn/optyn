bdc719197f3a2ff8a17f7ef79b331c80d7329a2a
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-05-29 17:48:05 +0530

    [72223284] Class renamed

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -270,7 +270,7 @@ class Shop < ActiveRecord::Base
   end
 
   def message_address
-    "<p class='optyn-paragraph'>#{name} | #{first_location_street_address} | #{first_location_city}, #{first_location_state_name} #{first_location_zip}</p>"
+    "<p class='ss-paragraph'>#{name} | #{first_location_street_address} | #{first_location_city}, #{first_location_state_name} #{first_location_zip}</p>"
   end
 
   def button_display?
