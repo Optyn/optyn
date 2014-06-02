fca3a3ccdd6869f72f5a6a4b2914764256200cf5
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-06-02 19:10:08 +0530

    Set the sending limits of Sendgrid

--- lib/email_counter.rb
+++ lib/email_counter.rb
@@ -1,6 +1,9 @@
 class EmailCounter
   SES_KEY = "SES"
   SENDGRID_KEY = "SENDGRID"
+  SENDGRID_SEND_LIMIT = "SENDGRID_SEND_LIMIT"
+
+
   class << self
     def increment_ses
       REDIS.incr(SES_KEY)
@@ -10,12 +13,20 @@ class EmailCounter
       REDIS.incr(SENDGRID_KEY)
     end
 
+    def initialize_if
+      REDIS[SENDGRID_SEND_LIMIT] = "1000" if REDIS[SENDGRID_SEND_LIMIT].to_i == 0    
+    end
+
+    def set_send_limit(val)
+      REDIS[SENDGRID_SEND_LIMIT] = val.to_s
+    end
+
     def fetch_email_delivery_type
-      if REDIS[SES_KEY].to_i <= REDIS[SENDGRID_KEY].to_i   
-        SES_KEY
-      elsif REDIS[SES_KEY].to_i > REDIS[SENDGRID_KEY].to_i
+      if REDIS[SENDGRID_KEY].to_i <= REDIS[SENDGRID_SEND_LIMIT].to_i
         SENDGRID_KEY
-      end
+      else
+        SES_KEY
+      end 
     end
 
   end #end of self block
