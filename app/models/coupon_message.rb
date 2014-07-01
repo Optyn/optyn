3db38b0a97f84a8f10f93a97d49b1ac8e9620fa2
Vignesh <vignesh20@gmail.com>
2014-07-01 22:21:02 +0530

    Update Coupon message to use message extension module

--- app/models/coupon_message.rb
+++ app/models/coupon_message.rb
@@ -1,14 +1,10 @@
 class CouponMessage < Message
-  has_one :extension, :class_name => 'CouponMessageExtension', :foreign_key => :message_id
+  has_extension :class_name => 'CouponMessageExtension', :foreign_key => :message_id,
+    :attrs => [:redemption_instructions]
 
-  attr_accessor :redemption_instructions
+  attr_accessible :type_of_discount, :discount_amount, :fine_print, :coupon_code, :content, :ending, :permanent_coupon
 
-  attr_accessible :type_of_discount, :discount_amount, :fine_print, :coupon_code, :content, :ending,
-    :permanent_coupon, :redemption_instructions
-
-  after_initialize :load_extension
-  before_save :assign_coupon_code, :save_extension
-  before_destroy :destroy_extension
+  before_save :assign_coupon_code
 
   with_options on: :update do |m|
     m.validate :validate_ending
@@ -16,25 +12,5 @@ class CouponMessage < Message
     m.validate :validate_discount_amount
     m.validates :content, presence: true
     m.validate :validate_ending
-  end
-
-  default_scope { includes :extension }
-
-  private
-
-  def load_extension
-    extension = self.extension || self.build_extension
-    @redemption_instructions = extension.redemption_instructions
-  end
-
-  def save_extension
-    extension = self.extension || self.build_extension
-    extension.redemption_instructions = @redemption_instructions
-    # Error management can be done here
-    extension.save
-  end
-
-  def destroy_extension
-    self.extension.destroy if self.extension.exists?
   end
 end
--- app/models/coupon_message_extension.rb
+++ app/models/coupon_message_extension.rb
@@ -1,3 +1,3 @@
 class CouponMessageExtension < ActiveRecord::Base
-  belongs_to :message
+  belongs_to :message, :class_name => 'CouponMessage'
 end
