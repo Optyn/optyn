eae401ee885727042b97ebd629ae4a0455af55d6
Vignesh <vignesh20@gmail.com>
2014-07-02 15:48:07 +0530

    Add fine print to sale message

--- app/models/sale_message.rb
+++ app/models/sale_message.rb
@@ -2,7 +2,7 @@ class SaleMessage < Message
   has_extension :class_name => 'SaleMessageExtension', :foreign_key => :message_id,
   	:attrs => [:redemption_instructions]
 
-  attr_accessible :content, :ending, :permanent_coupon
+  attr_accessible :content, :ending, :permanent_coupon, :fine_print
 
   with_options on: :update do |m|
     m.validates :content, presence: true
