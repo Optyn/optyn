2f4211a2ea930bb359de14e589b6ede80a28ecdc
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2014-07-07 12:14:35 +0530

    Refactored JS for Constant Contact Pricing slider.

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,22 +1,43 @@
 
-$( function() {
-  // Code for Constant Contact pricing section.
-  $( '#custom-price' ).keyup( function() {
-    if ( $( this ).val() < 0 ) {
-      $( '#op-price' ).text( '' );
-      return;
+var OP = OP || {};
+
+OP = (function($, window, doucument, Optyn){
+
+  Optyn.StaticPages = {};
+
+  Optyn.StaticPages.initialize = function() {
+    if ( $( '#cc-price' ).length ) {
+      this.constantContactPricingComparison();
     }
-    $( '#op-price' ).text( Math.floor( $( this ).val() / 2 ));
-  });
-  if ( $( '#cc-price' ).length ) {
-    // 
-    $( '#cc-price' ).change( function() {
-      if ( $( this ).val() === 'other' ) {
-        $( '.ccc-price' ).val( '' ).slideDown();
+  };
+
+  Optyn.StaticPages.constantContactPricingComparison = function() {
+    // Code for Constant Contact pricing section.
+    $( '#custom-price' ).keyup( function() {
+      if ( $( this ).val() < 0 ) {
+        $( '#op-price' ).text( '' );
         return;
       }
-      $( '.ccc-price' ).slideUp();
       $( '#op-price' ).text( Math.floor( $( this ).val() / 2 ));
     });
-  }
+    if ( $( '#cc-price' ).length ) {
+      // 
+      $( '#cc-price' ).change( function() {
+        if ( $( this ).val() === 'other' ) {
+          $( '.ccc-price' ).val( '' ).slideDown();
+          return;
+        }
+        $( '.ccc-price' ).slideUp();
+        $( '#op-price' ).text( Math.floor( $( this ).val() / 2 ));
+      });
+    }
+  };
+
+  return Optyn;
+})(jQuery, this, this.document, OP);
+
+
+
+$(document).ready(function(){
+  OP.StaticPages.initialize();
 });
