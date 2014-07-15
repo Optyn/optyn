ae626fc8e585928cda3fea6584b711d6807aa4ef
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2014-07-15 20:05:20 +0530

    74980610 | Made search subscribers form crisper.

--- app/assets/javascripts/merchants/filter.js
+++ app/assets/javascripts/merchants/filter.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   if ($('#filter_Form .chzn-select').length) {
-    $('.chzn-select').chosen();
+    $('.chzn-select').chosen({
+      width: '30%'
+    });
   }
   if ( $('#toggle-search-form-visibility').length) {
     $( '#toggle-search-form-visibility' ).click( function() {
