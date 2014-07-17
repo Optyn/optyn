630a5cee1f0ee643f840ad562f6dbad718f25018
Gaurav Gaglani <gaurav.gaglani@gmail.com>
2014-07-17 15:57:38 +0530

    Making the dimensions protected access scope

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 server "ec2-23-22-29-198.compute-1.amazonaws.com", :web, :app, :db, :messenger, primary: true
-set :branch, "development"
+set :branch, "master"
 set :rails_env, 'staging'
 set :local_app_url, 'http://localhost:3000/'
 set :workers, {"*" => 1}
